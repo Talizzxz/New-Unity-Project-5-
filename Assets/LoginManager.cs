230262b90f017045fdbed306777ed2d6f7be475d
Talizzxz <tota.mein@gmail.com>
2024-05-28 09:07:49 -0300

    terminado

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginManager : MonoBehaviour
+{
+    public string contraseña;
+    public InputField imputContraseña;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ComoVosDesees()
+    {
+        if(imputContraseña.text == contraseña)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
